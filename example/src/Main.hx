e228a9ecf5bbf74cc19d41d1797e2c2fbaef1e9f
Alexander Kuzmenko <alex@stablex.ru>
2015-10-03 00:47:31 +0300

    widget position & size changed to abstracts

--- example/src/Main.hx
+++ example/src/Main.hx
@@ -26,16 +26,16 @@ class Main
         Lib.current.stage.align     = flash.display.StageAlign.TOP_LEFT;
 
         var root = new Widget();
-        root.left.px = 200;
-        root.top.px  = 200;
-        root.width.px  = 100;
-        root.height.px = 30;
+        root.left = 200;
+        root.top  = 200;
+        root.width = 100;
+        root.height = 30;
         root.origin.set(1, 0.5);
         root.skin = skin(Std.random(0xFFFFFF));
 
         var child = root.addChild(new Widget());
-        child.left.px = 50;
-        child.bottom.px = -15;
+        child.left = 50;
+        child.bottom = -15;
         child.width.pct = 100;
         child.height.pct = 100;
         child.skin = skin(Std.random(0xFFFFFF));
@@ -46,9 +46,9 @@ class Main
         //left aligned text
         var label1 = new Text();
         label1.text      = 'Left aligned';
-        label1.left.dip  = 10;
-        label1.top.dip   = 10;
-        label1.width.dip = 100;
+        label1.left  = 10;
+        label1.top   = 10;
+        label1.width = 100;
         label1.skin      = skin(Std.random(0xFFFFFF));
         var format = label1.getTextFormat();
         format.align = TextFormatAlign.LEFT;
@@ -58,9 +58,9 @@ class Main
         //center aligned text
         var label2 = new Text();
         label2.text      = 'Center aligned';
-        label2.left.dip  = 150;
-        label2.top.dip   = 10;
-        label2.width.dip = 100;
+        label2.left  = 150;
+        label2.top   = 10;
+        label2.width = 100;
         label2.skin      = skin(Std.random(0xFFFFFF));
         var format = label2.getTextFormat();
         format.align = TextFormatAlign.CENTER;
@@ -70,9 +70,9 @@ class Main
         //right aligned text
         var label3 = new Text();
         label3.text      = 'Right aligned';
-        label3.left.dip  = 300;
-        label3.top.dip   = 10;
-        label3.width.dip = 100;
+        label3.left  = 300;
+        label3.top   = 10;
+        label3.width = 100;
         label3.skin      = skin(Std.random(0xFFFFFF));
         var format = label3.getTextFormat();
         format.align = TextFormatAlign.RIGHT;
@@ -83,8 +83,8 @@ class Main
         var data = new BitmapData(100, 50);
         data.perlinNoise(100, 80, 6, Math.floor(Math.random() * 10), false, true);
         bmp.bitmapData = data;
-        bmp.left.px    = 500;
-        bmp.top.px     = 400;
+        bmp.left    = 500;
+        bmp.top     = 400;
         bmp.padding.px = 10;
         bmp.skin = skin(Std.random(0xFFFFFF));
         bmp.keepAspect = false;
@@ -93,15 +93,15 @@ class Main
         var a = 0.;
         var labelWidth = 0.;
         Lib.current.addEventListener(Event.ENTER_FRAME, function(_){
-            label1.width.px = 100 + 25 * Math.sin(a);
-            label2.width.px = 100 + 25 * Math.cos(a);
-            label3.width.px = 100 + 25 * Math.sin(a);
+            label1.width = 100 + 25 * Math.sin(a);
+            label2.width = 100 + 25 * Math.cos(a);
+            label3.width = 100 + 25 * Math.sin(a);
 
-            // bmp.width.px  = 100 + 25 * Math.cos(a);
-            // bmp.height.px = 50 + 25 * Math.sin(a);
+            // bmp.width  = 100 + 25 * Math.cos(a);
+            // bmp.height = 50 + 25 * Math.sin(a);
 
             root.rotation += 0.2;
-            root.width.px = 100 + 20 * Math.sin(a);
+            root.width = 100 + 20 * Math.sin(a);
             root.scaleY = 1 + 0.5 * Math.cos(a);
 
             child.alpha = 0.5 + 0.5 * Math.sin(2 * a);
