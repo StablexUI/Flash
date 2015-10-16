8057d28a731fe233cf9629ac44f3d6d954545a1c
Alexander Kuzmenko <alex@stablex.ru>
2015-10-16 13:08:50 +0300

    example update

--- example/src/Main.hx
+++ example/src/Main.hx
@@ -8,6 +8,7 @@ import sx.layout.LineLayout;
 import sx.skins.PaintSkin;
 import sx.Sx;
 import sx.themes.flatui.ButtonStyle;
+import sx.themes.flatui.ProgressStyle;
 import sx.themes.flatui.TextInputStyle;
 import sx.themes.FlatUITheme;
 import sx.tween.easing.*;
@@ -143,10 +144,10 @@ class Main
      */
     static public function addBars () : Void
     {
-        var randomValue = function (p:Progress) return p.min + (0.1 + 0.9 * Math.random()) * (p.max - p.min);
+        var randomValue = function (p:Progress) return p.min + (0.2 + 0.6 * Math.random()) * (p.max - p.min);
 
         var box = new VBox();
-        box.gap     = 30;
+        box.gap     = 20;
         box.padding = 10;
         box.left    = 250;
         box.top     = 100;
@@ -154,9 +155,111 @@ class Main
         var progress = new Progress();
         progress.value  = randomValue(progress);
         progress.easing = Quad.easeOut;
-        progress.onPointerPress.add(function(_,_,_) progress.value = randomValue(progress));
+        progress.interactive = true;
         box.addChild(progress);
 
+        var progress = new Progress();
+        progress.style  = ProgressStyle.WARNING;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        box.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.CONCRETE;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        box.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.DANGER;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        box.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.SUCCESS;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.right.select();
+        box.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.INVERSE;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.right.select();
+        box.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.INFO;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.right.select();
+        box.addChild(progress);
+
+        var hbox = new HBox();
+        hbox.gap = 20;
+        hbox.padding = 10;
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.WARNING_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.CONCRETE_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.DANGER_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.SUCCESS_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.top.select();
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.INVERSE_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.top.select();
+        hbox.addChild(progress);
+
+        var progress = new Progress();
+        progress.style  = ProgressStyle.INFO_VERTICAL;
+        progress.value  = randomValue(progress);
+        progress.easing = Quad.easeOut;
+        progress.interactive = true;
+        progress.bar.top.select();
+        hbox.addChild(progress);
+
+        box.addChild(hbox);
         Sx.root.addChild(box);
     }
 
