9062c64ffb5a1c63976a35d64a9ee8e84ccdd6ad
Alexander Kuzmenko <alex@stablex.ru>
2015-11-19 01:44:28 +0300

    Slice9Skin refactoring

--- src/sx/backend/flash/skins/Slice9Skin.hx
+++ src/sx/backend/flash/skins/Slice9Skin.hx
@@ -17,6 +17,26 @@ using sx.tools.PropertiesTools;
  */
 class Slice9Skin extends Slice9SkinBase
 {
+    /** For internal usage */
+    private var __drawRect : Rectangle = null;
+    private var __srcRect  : Rectangle = null;
+    private var __src      : Rectangle = null;
+    private var __dst      : Rectangle = null;
+
+
+    /**
+     * Constructor
+     */
+    public function new () : Void
+    {
+        super();
+
+        __srcRect  = new Rectangle();
+        __drawRect = new Rectangle();
+        __src      = new Rectangle();
+        __dst      = new Rectangle();
+    }
+
 
     /**
      * Called when skin visualization should be updated because of widget changes.
@@ -33,149 +53,201 @@ class Slice9Skin extends Slice9SkinBase
         }
         canvas.graphics.clear();
 
-        var drawRect = new Rectangle(
-            padding.left.px,
-            padding.top.px,
-            __widget.width.px - padding.sumPx(Horizontal),
-            __widget.height.px - padding.sumPx(Vertical)
-        );
-        var srcRect = bitmapData.rect;
+        __drawRect.x      = padding.left.px;
+        __drawRect.y      = padding.top.px;
+        __drawRect.width  = __widget.width.px - padding.sumPx(Horizontal);
+        __drawRect.height = __widget.height.px - padding.sumPx(Vertical);
 
-        var src = new Rectangle();
-        var dst = new Rectangle();
+        __srcRect.width = bitmapData.width;
+        __srcRect.height = bitmapData.height;
 
-        var scaleX : Float = (drawRect.width >= srcRect.width ? 1 : drawRect.width / srcRect.width);
-        var scaleY : Float = (drawRect.height >= srcRect.height ? 1 : drawRect.height / srcRect.height);
+        var scaleX : Float = (__drawRect.width >= __srcRect.width ? 1 : __drawRect.width / __srcRect.width);
+        var scaleY : Float = (__drawRect.height >= __srcRect.height ? 1 : __drawRect.height / __srcRect.height);
         //do not draw anything
         if( scaleX <= 0 || scaleY <= 0 ){
             return;
         }
 
-        //top left{
-            src.x      = srcRect.x;
-            src.y      = srcRect.y;
-            src.width  = _sliceSize(slice[0], srcRect.width);
-            src.height = _sliceSize(slice[2], srcRect.height);
+        __leftTop(slice[0], slice[2], scaleX, scaleY);
+        __centerTop(slice[0], slice[1], slice[2], scaleX, scaleY);
+        __rightTop(slice[1], slice[2], scaleX, scaleY);
+        __leftMiddle(slice[0], slice[2], slice[3], scaleX, scaleY);
+        __centerMiddle(slice[0], slice[1], slice[2], slice[3], scaleX, scaleY);
+        __rightMiddle(slice[1], slice[2], slice[3], scaleX, scaleY);
+        __leftBottom(slice[0], slice[3], scaleX, scaleY);
+        __centerBottom(slice[0], slice[1], slice[3], scaleX, scaleY);
+        __rightBottom(slice[1], slice[2], slice[3], scaleX, scaleY);
+    }
 
-            dst.x      = drawRect.x;
-            dst.y      = drawRect.y;
-            dst.width  = _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.height = _sliceSize(slice[2], srcRect.height) * scaleY;
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+    /**
+     * Draw left-top part of image
+     */
+    private function __leftTop (left:Float, top:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x;
+        __src.y      = __srcRect.y;
+        __src.width  = __sliceSize(left, __srcRect.width);
+        __src.height = __sliceSize(top, __srcRect.height);
 
-        //top middle{
-            src.x      = srcRect.x + _sliceSize(slice[0], srcRect.width);
-            src.y      = srcRect.y;
-            src.width  = _sliceSize(slice[1] - slice[0], srcRect.width);
-            src.height = _sliceSize(slice[2], srcRect.height);
+        __dst.x      = __drawRect.x;
+        __dst.y      = __drawRect.y;
+        __dst.width  = __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.height = __sliceSize(top, __srcRect.height) * scaleY;
 
-            dst.x      = drawRect.x + _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.y      = drawRect.y;
-            dst.width  = drawRect.width - (_sliceSize(slice[0], srcRect.width) + (srcRect.width - _sliceSize(slice[1], srcRect.width))) * scaleX;
-            dst.height = _sliceSize(slice[2], srcRect.height) * scaleY;
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
 
-        //top right{
-            src.x      = srcRect.x + _sliceSize(slice[1], srcRect.width);
-            src.y      = srcRect.y;
-            src.width  = srcRect.width - _sliceSize(slice[1], srcRect.width);
-            src.height = _sliceSize(slice[2], srcRect.height);
+    /**
+     * Draw center-top part of image
+     */
+    private function __centerTop (left:Float, right:Float, top:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(left, __srcRect.width);
+        __src.y      = __srcRect.y;
+        __src.width  = __sliceSize(right - left, __srcRect.width);
+        __src.height = __sliceSize(top, __srcRect.height);
 
-            dst.x      = drawRect.x + drawRect.width - src.width * scaleX;
-            dst.y      = drawRect.y;
-            dst.width  = src.width * scaleX;
-            dst.height = _sliceSize(slice[2], srcRect.height) * scaleY;
+        __dst.x      = __drawRect.x + __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.y      = __drawRect.y;
+        __dst.width  = __drawRect.width - (__sliceSize(left, __srcRect.width) + (__srcRect.width - __sliceSize(right, __srcRect.width))) * scaleX;
+        __dst.height = __sliceSize(top, __srcRect.height) * scaleY;
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-        //middle left{
-            src.x      = srcRect.x;
-            src.y      = srcRect.y + _sliceSize(slice[2], srcRect.height);
-            src.width  = _sliceSize(slice[0], srcRect.width);
-            src.height = _sliceSize(slice[3] - slice[2], srcRect.height);
 
-            dst.x      = drawRect.x;
-            dst.y      = drawRect.y + _sliceSize(slice[2], srcRect.height) * scaleY;
-            dst.width  = _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.height = drawRect.height - (_sliceSize(slice[2], srcRect.height) + (srcRect.height - _sliceSize(slice[3], srcRect.height))) * scaleY;
+    /**
+     * Draw right-top part of image
+     */
+    private function __rightTop (right:Float, top:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(right, __srcRect.width);
+        __src.y      = __srcRect.y;
+        __src.width  = __srcRect.width - __sliceSize(right, __srcRect.width);
+        __src.height = __sliceSize(top, __srcRect.height);
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+        __dst.x      = __drawRect.x + __drawRect.width - __src.width * scaleX;
+        __dst.y      = __drawRect.y;
+        __dst.width  = __src.width * scaleX;
+        __dst.height = __sliceSize(top, __srcRect.height) * scaleY;
 
-        //middle middle{
-            src.x      = srcRect.x + _sliceSize(slice[0], srcRect.width);
-            src.y      = srcRect.y + _sliceSize(slice[2], srcRect.height);
-            src.width  = _sliceSize(slice[1] - slice[0], srcRect.width);
-            src.height = _sliceSize(slice[3] - slice[2], srcRect.height);
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-            dst.x      = drawRect.x + _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.y      = drawRect.y + _sliceSize(slice[2], srcRect.height) * scaleY;
-            dst.width  = drawRect.width - (_sliceSize(slice[0], srcRect.width) + (srcRect.width - _sliceSize(slice[1], srcRect.width))) * scaleX;
-            dst.height = drawRect.height - (_sliceSize(slice[2], srcRect.height) + (srcRect.height - _sliceSize(slice[3], srcRect.height))) * scaleY;
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+    /**
+     * Draw left-middle part of image
+     */
+    private function __leftMiddle (left:Float, top:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x;
+        __src.y      = __srcRect.y + __sliceSize(top, __srcRect.height);
+        __src.width  = __sliceSize(left, __srcRect.width);
+        __src.height = __sliceSize(bottom - top, __srcRect.height);
 
-        //middle right{
-            src.x      = srcRect.x + _sliceSize(slice[1], srcRect.width);
-            src.y      = srcRect.y + _sliceSize(slice[2], srcRect.height);
-            src.width  = srcRect.width - _sliceSize(slice[1], srcRect.width);
-            src.height = _sliceSize(slice[3] - slice[2], srcRect.height);
+        __dst.x      = __drawRect.x;
+        __dst.y      = __drawRect.y + __sliceSize(top, __srcRect.height) * scaleY;
+        __dst.width  = __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.height = __drawRect.height - (__sliceSize(top, __srcRect.height) + (__srcRect.height - __sliceSize(bottom, __srcRect.height))) * scaleY;
 
-            dst.x      = drawRect.x + drawRect.width - src.width * scaleX;
-            dst.y      = drawRect.y + _sliceSize(slice[2], srcRect.height) * scaleY;
-            dst.width  = src.width * scaleX;
-            dst.height = drawRect.height - (_sliceSize(slice[2], srcRect.height) + (srcRect.height - _sliceSize(slice[3], srcRect.height))) * scaleY;
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
 
-        //bottom left{
-            src.x      = srcRect.x;
-            src.y      = srcRect.y + _sliceSize(slice[3], srcRect.height);
-            src.width  = _sliceSize(slice[0], srcRect.width);
-            src.height = srcRect.height - _sliceSize(slice[3], srcRect.height);
+    /**
+     * Draw center-Middle part of image
+     */
+    private function __centerMiddle (left:Float, right:Float, top:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(left, __srcRect.width);
+        __src.y      = __srcRect.y + __sliceSize(top, __srcRect.height);
+        __src.width  = __sliceSize(right - left, __srcRect.width);
+        __src.height = __sliceSize(bottom - top, __srcRect.height);
 
-            dst.x      = drawRect.x;
-            dst.y      = drawRect.y + drawRect.height - src.height * scaleY;
-            dst.width  = _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.height = src.height * scaleY;
+        __dst.x      = __drawRect.x + __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.y      = __drawRect.y + __sliceSize(top, __srcRect.height) * scaleY;
+        __dst.width  = __drawRect.width - (__sliceSize(left, __srcRect.width) + (__srcRect.width - __sliceSize(right, __srcRect.width))) * scaleX;
+        __dst.height = __drawRect.height - (__sliceSize(top, __srcRect.height) + (__srcRect.height - __sliceSize(bottom, __srcRect.height))) * scaleY;
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-        //bottom middle{
-            src.x      = srcRect.x + _sliceSize(slice[0], srcRect.width);
-            src.y      = srcRect.y + _sliceSize(slice[3], srcRect.height);
-            src.width  = _sliceSize(slice[1] - slice[0], srcRect.width);
-            src.height = srcRect.height - _sliceSize(slice[3], srcRect.height);
 
-            dst.x      = drawRect.x + _sliceSize(slice[0], srcRect.width) * scaleX;
-            dst.y      = drawRect.y + drawRect.height - src.height * scaleY;
-            dst.width  = drawRect.width - (_sliceSize(slice[0], srcRect.width) + (srcRect.width - _sliceSize(slice[1], srcRect.width))) * scaleX;
-            dst.height = src.height * scaleY;
+    /**
+     * Draw right-Middle part of image
+     */
+    private function __rightMiddle (right:Float, top:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(right, __srcRect.width);
+        __src.y      = __srcRect.y + __sliceSize(top, __srcRect.height);
+        __src.width  = __srcRect.width - __sliceSize(right, __srcRect.width);
+        __src.height = __sliceSize(bottom - top, __srcRect.height);
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+        __dst.x      = __drawRect.x + __drawRect.width - __src.width * scaleX;
+        __dst.y      = __drawRect.y + __sliceSize(top, __srcRect.height) * scaleY;
+        __dst.width  = __src.width * scaleX;
+        __dst.height = __drawRect.height - (__sliceSize(top, __srcRect.height) + (__srcRect.height - __sliceSize(bottom, __srcRect.height))) * scaleY;
 
-        //bottom right{
-            src.x      = srcRect.x + _sliceSize(slice[1], srcRect.width);
-            src.y      = srcRect.y + _sliceSize(slice[3], srcRect.height);
-            src.width  = srcRect.width - _sliceSize(slice[1], srcRect.width);
-            src.height = srcRect.height - _sliceSize(slice[3], srcRect.height);
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
 
-            dst.x      = drawRect.x + drawRect.width - src.width * scaleX;
-            dst.y      = drawRect.y + drawRect.height - src.height * scaleY;
-            dst.width  = src.width * scaleX;
-            dst.height = src.height * scaleY;
 
-            __skinDrawSlice(bitmapData, src, dst);
-        //}
+    /**
+     * Draw left-Bottom part of image
+     */
+    private function __leftBottom (left:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x;
+        __src.y      = __srcRect.y + __sliceSize(bottom, __srcRect.height);
+        __src.width  = __sliceSize(left, __srcRect.width);
+        __src.height = __srcRect.height - __sliceSize(bottom, __srcRect.height);
+
+        __dst.x      = __drawRect.x;
+        __dst.y      = __drawRect.y + __drawRect.height - __src.height * scaleY;
+        __dst.width  = __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.height = __src.height * scaleY;
+
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
+
+
+    /**
+     * Draw center-Bottom part of image
+     */
+    private function __centerBottom (left:Float, right:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(left, __srcRect.width);
+        __src.y      = __srcRect.y + __sliceSize(bottom, __srcRect.height);
+        __src.width  = __sliceSize(right - left, __srcRect.width);
+        __src.height = __srcRect.height - __sliceSize(bottom, __srcRect.height);
+
+        __dst.x      = __drawRect.x + __sliceSize(left, __srcRect.width) * scaleX;
+        __dst.y      = __drawRect.y + __drawRect.height - __src.height * scaleY;
+        __dst.width  = __drawRect.width - (__sliceSize(left, __srcRect.width) + (__srcRect.width - __sliceSize(right, __srcRect.width))) * scaleX;
+        __dst.height = __src.height * scaleY;
+
+        __skinDrawSlice(bitmapData, __src, __dst);
+    }
+
+
+    /**
+     * Draw right-bottom part of image
+     */
+    private function __rightBottom (right:Float, top:Float, bottom:Float, scaleX:Float, scaleY:Float) : Void
+    {
+        __src.x      = __srcRect.x + __sliceSize(right, __srcRect.width);
+        __src.y      = __srcRect.y + __sliceSize(bottom, __srcRect.height);
+        __src.width  = __srcRect.width - __sliceSize(right, __srcRect.width);
+        __src.height = __srcRect.height - __sliceSize(bottom, __srcRect.height);
+
+        __dst.x      = __drawRect.x + __drawRect.width - __src.width * scaleX;
+        __dst.y      = __drawRect.y + __drawRect.height - __src.height * scaleY;
+        __dst.width  = __src.width * scaleX;
+        __dst.height = __src.height * scaleY;
+
+        __skinDrawSlice(bitmapData, __src, __dst);
     }
 
 
